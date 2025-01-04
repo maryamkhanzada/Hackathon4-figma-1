7f6a3cf973ff4582e5a1372f78e742c8da8f20b9
maryamkhanzada <khanzadamuzammil112@gmail.com>
2025-01-05 00:16:43 +0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -54,7 +54,7 @@ const Header = () => {
                 <div className="w-full lg:w-auto">
                     <Image
                         src="/sofa 1.png"
-                        alt="chair"
+                        alt="sofa"
                         width={200}
                         height={200}
                         className="w-full lg:w-[696px] h-auto"
