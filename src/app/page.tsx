ef9dedb0b905bd0451007b7718ce6e567927e46b
codewithmaryam1418 <maryamahmedkhan78@gmail.com>
2025-02-07 18:22:43 +0500

    new update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,18 @@ import Header from "./components/Header";
 import Footer from "./components/footer";
 import { ImageSection, Hero, BlogSection, Hero1 } from "./components/hero";
 import ProductCards from "./products/page";
+ 
 
-
-
-  export default function Home() {
-      return(
+export default function Home() {
+  return (
     <div>
       <Header />
       <ImageSection />
       <Hero />
       <BlogSection />
       <Hero1 />
-      <ProductCards  />
-         
-
-      <Footer /> 
+      <ProductCards />
+      <Footer />
     </div>
   );
 }
